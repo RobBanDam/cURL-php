2d0f2828760bc80c7cef39c7f7eb8674f09cddd6
RobBanDam <robert.mtz.a.08@gmail.com>
2024-01-17 01:32:40 -0600

    17/01/2024 Validando datos Obtenidos

--- convertir.php
+++ convertir.php
@@ -1,15 +1,28 @@
-<?php 
+<?php
 
 if(isset($_POST['valor'])){
     $valor = floatval($_POST['valor']);
 
-    $apiURL = "https://v6.exchangerate-api.com/v6/9b32f3dec9f1bcb12bf6ad55/latest/USD";
+    $apiURL = "https://v6.exchangerate-api.com/v6/f86192bd0e1f2cb13120576e/latest/USD";
 
     $iniciarCURL = curl_init($apiURL);
+
+    // Desactivar la verificación SSL (NO RECOMENDADO)
+    curl_setopt($iniciarCURL, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($iniciarCURL, CURLOPT_SSL_VERIFYHOST, false);
 
     curl_setopt($iniciarCURL, CURLOPT_RETURNTRANSFER, true);
 
     $respuesta = curl_exec($iniciarCURL);
-}
 
+    if(curl_errno($iniciarCURL)){
+        echo "Error al realizar la solicitud: " . curl_error($iniciarCURL);
+        exit;
+    }
+
+    // Cerrar la sesión cURL
+    curl_close($iniciarCURL);
+
+    $datos = json_decode($respuesta, true);
+}
 ?>
